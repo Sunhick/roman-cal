9052b3f4a8db293694ee63baf09969149ef6ab5a
Sunil <sunhick@gmail.com>
2017-01-23 14:04:21 -0700

    adding unit test files

---
+++ tests/check_add.c
@@ -0,0 +1,10 @@
+/* check_add.c -- unit testcases for addition of roman numerals
+ *
+ * Copyright (C) 2017 Sunil <sunhick@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+#include <check.h>
+
---
+++ tests/check_sub.c
@@ -0,0 +1,9 @@
+/* check_add.c -- unit testcases for subtraction of roman numerals
+ *
+ * Copyright (C) 2017 Sunil <sunhick@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+#include <check.h>
