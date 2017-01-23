bca5b4d06d02175ed22d3abf320dcdd53aca2953
Sunil <sunhick@gmail.com>
2017-01-23 15:38:53 -0700

    added dummy impl file

---
+++ src/rmcalc.c
@@ -0,0 +1,10 @@
+/* rmcalc.c -- implementation file for roman calculator. 
+ *
+ * Copyright (C) 2017 Sunil <sunhick@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+#include "headers/rmcalc.h"
+
